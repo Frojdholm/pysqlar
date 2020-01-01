60939496aa1ebf7c3a5b0176d9a5de9298515d2b
Hampus Fröjdholm <hampus.frojdholm@gmail.com>
2020-01-01 15:03:23 +0100

    Add license and setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+this_folder = Path(__file__).parent
+with open(this_folder / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name='pysqlar',
+    version='0.1',
+    description='Pure Python library for working with SQLite Archives.',
+    long_description=long_description,
+    url='http://github.com/Frojdholm/pysqlar',
+    author='Hampus Fröjdholm',
+    author_email='hampus.frojdholm@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    zip_safe=True
+)
