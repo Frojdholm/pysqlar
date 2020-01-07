b96da2039f1f12908b77aed9eda158f2730ca7cc
Hampus Fröjdholm <hampus.frojdholm@gmail.com>
2020-01-07 02:16:43 +0100

    Make package ready for distribution

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from setuptools import setup, find_packages
-from pathlib import Path
-
-
-this_folder = Path(__file__).parent
-with open(this_folder / "README.md", encoding="utf-8") as f:
-    long_description = f.read()
 
 
 setup(
     name='pysqlar',
     version='0.1',
     description='Pure Python library for working with SQLite Archives.',
-    long_description=long_description,
+    long_description="""# pysqlar
+
+Module for working with [SQLite Archive files](https://www.sqlite.org/sqlar.html)
+with an API mimicking the zipfile module.
+
+The module requires the zlib and sqlite3 support, but has no external dependencies.
+""",
+    long_description_content_type='text/markdown',
     url='http://github.com/Frojdholm/pysqlar',
     author='Hampus Fröjdholm',
     author_email='hampus.frojdholm@gmail.com',
     license='MIT',
     packages=find_packages(),
+    python_requires='>=3',
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3"
